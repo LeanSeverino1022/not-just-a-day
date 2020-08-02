1d58ed9277686da669d6b4ba87d97ed6e77edb0c
Puyih Severino <yihseverino@gmail.com>
2020-08-03 02:24:08 +0800

    save with local storage, toggle done/not-done items

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
 import todosData from './data';
 import { v4 as uuidv4 } from 'uuid';
 
+const LOCAL_STORAGE_KEY = 'oneDayApp.todos';
+
 function App () {
-  const [todos, setTodos] = useState(todosData)
-  const todoNameRef = useRef(null)
+  const [todos, setTodos] = useState(todosData);
+
+  const todoNameRef = useRef(null);
+
+  //do once, when our component loads
+  useEffect(_=>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+    if (storedTodos) setTodos(storedTodos);
+  }, [])
+
+  //when something changes, do something
+  useEffect(_=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos])
 
   function addTodoItem () {
 
-    const name = todoNameRef.current.value
+    const name = todoNameRef.current.value.trim();
     if (name === '') return;
 
     setTodos(prevTodos => {
-
-      var newTodos = prevTodos.concat({ name: "gago", id:10, complete:false})
-      return newTodos;
+        return [...prevTodos, { name: name, id:uuidv4 (), complete:false}];
     });
+  }
 
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
 
+  function clearTodos () {
+    setTodos([]);
   }
 
   return (
     <React.Fragment>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input type='text' ref={todoNameRef} />
       <button onClick={addTodoItem}>Add Todo</button>
-      <button>Clear Completed Todos</button>
+      <button onClick={clearTodos}>Clear Completed Todos</button>
       <div>0 left to do</div>
     </React.Fragment>
   )
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,12 +2,11 @@ import React from 'react';
 import Todo from './Todo.js'
 
 
-export default function TodoList({ todos }) {
-
+export default function TodoList({ todos, toggleTodo }) {
     return (
         <div>
             {todos.map( todo => {
-                return <Todo key={todo.id} todo={todo}/>
+                return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo}/>
             })}
         </div>
 

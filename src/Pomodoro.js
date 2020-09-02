528acae51214ba9452a199bd4d08fd39a5a79b1f
Leandro Severino Jr <yihseverino@gmail.com>
2020-09-02 23:59:33 +0800

    timer fix attempt

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -2,60 +2,76 @@ import React, {useRef, useState, useEffect} from 'react'
 
 export default function Pomodoro() {
 
-    let startingMinutes = .5;
-    let timerIsRunning = false;
+    let startingMinutes = .15;
+
     let [totalSeconds, setTotalSeconds] = useState(startingMinutes * 60);
     let [timerUi, setTimerUI] = useState(null);
-    // let [timerIsRunning, toggleTimerIsRunning]  = false;
+    let [isTimerRunning, setIsTimerRunning]  = useState(false);
+    let [countdownInterval, setCountdownInterval] = useState(null);
 
-
-    let interval = null;
 
     function startTimer() {
-        interval = setInterval(continueCountdown, 1000);
+
+        if (!isTimerRunning) {
+            // interval = setInterval(continueCountdown, 1000);
+            // setCountdownInterval(setInterval(continueCountdown, 1000));
+            console.log("setCountdownInterval")
+            setIsTimerRunning(true);
+        }
+    }
+
+    function resetTimer() {
+        stopTimer();
+        setTotalSeconds(startingMinutes * 60);
+        alert('reset')
     }
 
     function stopTimer() {
-        clearInterval(interval);
-        timerIsRunning = false;
+        clearInterval(countdownInterval);
+        // setIsTimerRunning(false);
+    }
+
+    /**
+     * convert seconds(num) to minutes:seconds(string)
+     */
+    function convertSecondsToMinutesSecondsText(numSeconds) {
+         //seconds to MM-SS format
+        const minutesDisplay = Math.floor(numSeconds / 60);
+        numSeconds = (numSeconds % 60);
+        const secondsDisplay = numSeconds < 10 ? '0' + numSeconds : numSeconds;
+
+        return `${minutesDisplay}:${secondsDisplay}`;
     }
 
     // const countdownEl = document.getElementById('countdown');
 
     function continueCountdown() {
 
-        if (totalSeconds < 0 ) {
+        if (totalSeconds < 1 ) {
             alert("TIMES UP");
             stopTimer();
             return;
         }
 
-        //seconds to MM-SS format
-        const minutes = Math.floor(totalSeconds / 60);
+        setTotalSeconds(--totalSeconds);
 
-        let seconds = (totalSeconds % 60);
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        // seconds = (seconds => seconds < 10 ? '0' + seconds : seconds)(totalSeconds % 60);
+        const time = convertSecondsToMinutesSecondsText(totalSeconds)
 
-        setTimerUI(`${minutes}:${seconds}`);
-
-        console.log(totalSeconds);
-
-        setTotalSeconds(totalSeconds--);
+        setTimerUI(time);
     }
 
     //do once, when our component loads
     useEffect(_ => {
+        const time = convertSecondsToMinutesSecondsText(totalSeconds)
 
-        const minutes = Math.floor(totalSeconds / 60);
-
-        let seconds = (totalSeconds % 60);
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-
-        setTimerUI(`${minutes}:${seconds}`);
+        setTimerUI(time);
 
     }, [])
 
+    useEffect(_ => {
+        const interval = setInterval(continueCountdown, 1000);
+          return () => clearInterval(interval);
+    },['isTimerRunning'])
 
 
     return (
@@ -64,7 +80,11 @@ export default function Pomodoro() {
                 <div className="flex-grow flex flex-col w-full">
                     <p className="py-2">Pomodoro Timer</p>
                     <p className="text-6xl">{timerUi}</p>
-                    <button onClick={startTimer} className="bg-gray-700 hover:bg-gray-600 text-white inline-block mx-auto mb-2 py-1 px-2">{timerIsRunning ? 'Reset' : 'Start'}</button>
+                    {isTimerRunning ? (
+                        <button onClick={resetTimer} className="bg-gray-700 hover:bg-gray-600 text-white inline-block mx-auto mb-2 py-1 px-2">Reset</button>
+                    ) : (
+                        <button onClick={startTimer} className="bg-green-500 hover:bg-green-700 text-white inline-block mx-auto mb-2 py-1 px-2" >Start</button>
+                    )}
                 </div>
 
                 <div className="flex-grow flex flex-col">
